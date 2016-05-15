365b39f77b45c2da5961b21b118c6371e043f73f
Laila Rodriguez <matricharia@gmail.com>
2016-05-15 03:01:44 +0000

    Created controller and idex page for inquiries

---
+++ app/controllers/inquiries_controller.rb
@@ -0,0 +1,5 @@
+class InquiriesController < ApplicationController
+
+	def index
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Relationship::Application.routes.draw do
+  root 'inquiries#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
