823bd330a5a059e70e4497852ee29904d805e418
Laila Rodriguez <matricharia@gmail.com>
2016-05-15 00:57:47 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Relationship::Application.config.secret_key_base = '8405dd40e67df20b9634db30802c16a9c1316c0f41599c6f9d38cbd0f20e116b5519c32d1e874e5cf61e7806d6e0afe43e2a7a5c386239c44030766e998efde5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Relationship::Application.config.session_store :cookie_store, key: '_relationship_session'
